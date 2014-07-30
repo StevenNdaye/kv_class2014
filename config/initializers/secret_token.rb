2cc9d7da2c994add7945a3f88aee86dbdeacd28a
StevenNdaye <stevenndaye@gmail.com>
2014-07-30 17:38:56 -0300

    001(Steven): Set up a project with monologue gem, the blog is up and running

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KvClass2014::Application.config.secret_key_base = '89d88980a02e066978ff5417ba9889ff7659f99a47b5a7367f8fd1b9f13b93cf5300319ad218557993d1a38ca8d7b8eee81b0b33013724a5487ededfe9ab5482'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KvClass2014::Application.config.session_store :cookie_store, key: '_kv_class2014_session'
